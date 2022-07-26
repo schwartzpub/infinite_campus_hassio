06f46e6e069d7fc35b073f87538c74e7abe2df24
schwartzpub <jake@schwartzpub.com>
2022-07-26 12:24:44 -0500

    update to adhere to standards

--- infinitehub.py
+++ infinitehub.py
@@ -1,24 +1,41 @@
 from __future__ import annotations
 
 import logging
+import aiohttp
 
-from typing import Any
+from typing import Any, Dict
 
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-import aiohttp
+from .const import (
+    CONF_USERNAME,
+    CONF_BASEURI,
+    CONF_DISTRICT,
+    CONF_SECRET,
+    DOMAIN,
+    SCAN_INTERVAL
+)
 
 _LOGGER = logging.getLogger(__name__)
 
-class InfiniteHub:
-    def __init__(self, hass: HomeAssistant, district: str, baseuri: str, username: str, password: str) -> None:
+class InfiniteHub(DataUpdateCoordinator[Dict[str, Any]]):
+    def __init__(
+        self,
+        hass: HomeAssistant
+    ) -> None:
         """Initialize."""
-        self._district = district
-        self._baseuri = baseuri
-        self._hass = hass
-        self._username = username
-        self._secret = password
-        self._id = district.lower()
+        super().__init__(
+            hass,
+            _LOGGER,
+            name=DOMAIN,
+            update_interval=SCAN_INTERVAL,
+        )
+
+        self._baseuri = self.config_entry.data[CONF_BASEURI]
+        self._username = self.config_entry.data[CONF_USERNAME]
+        self._secret = self.config_entry.data[CONF_SECRET]
+        self._district = self.config_entry.data[CONF_DISTRICT]
 
     async def authenticate(self) -> bool:
         """Test if we can authenticate with the district."""
@@ -31,12 +48,17 @@ class InfiniteHub:
                     return False
         
     async def poll_data(self,type) -> str:
-        async with aiohttp.ClientSession() as session:
-            async with session.post('{0}/campus/verify.jsp?nonBrowser=true&username={1}&password={2}&appName={3}&portalLoginPage={4}'.format(self._baseuri,self._username,self._secret,self._district,'parents')) as authresponse:
-                response = authresponse
-                if response.status == 200 and "password-error" not in await response.text():
-                    async with session.get('{0}/campus/resources/prism/portal/familyInfo'.format(self._baseuri)) as studentresp:
-                        studentresponse = await studentresp.text()
-                        return studentresponse
-                else:
-                    return False
+        if type == "student":
+            async with aiohttp.ClientSession() as session:
+                async with session.post('{0}/campus/verify.jsp?nonBrowser=true&username={1}&password={2}&appName={3}&portalLoginPage={4}'.format(self._baseuri,self._username,self._secret,self._district,'parents')) as authresponse:
+                    response = authresponse
+                    if response.status == 200 and "password-error" not in await response.text():
+                        async with session.get('{0}/campus/resources/prism/portal/familyInfo'.format(self._baseuri)) as studentresp:
+                            studentresponse = await studentresp.text()
+                            return studentresponse
+                    else:
+                        return False
+        elif type == "course":
+            return False
+        elif type == "assignment":
+            return False
--- sensor.py
+++ sensor.py
@@ -14,23 +14,25 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
-from .const import DOMAIN
+from .const import (
+    SCAN_INTERVAL,
+    DOMAIN
+)
 
 import logging
 _LOGGER = logging.getLogger(__name__)
 
-CONF_BASEURI= "baseuri"
-CONF_USERNAME= "username"
-CONF_SECRET= "password"
-CONF_DISTRICT= "district"
-
-SCAN_INTERVAL = timedelta(minutes=5)
+SCAN_INTERVAL = SCAN_INTERVAL
 
 async def async_setup_entry(hass,config_entry,async_add_entities):
     """Set up the sensor platform."""
     hub = hass.data[DOMAIN][config_entry.entry_id]
 
-    async_add_entities([InfiniteStudentSensor(hass,hub)])
+    async_add_entities(
+        [InfiniteStudentSensor(hass,hub)],
+        [InfiniteCourseSensor(hass,hub)],
+        [InfiniteAssignmentSensor(hass,hub)]
+    )
 
 class InfiniteStudentSensor(SensorEntity):
     def __init__(
@@ -45,43 +47,58 @@ class InfiniteStudentSensor(SensorEntity):
         self._attr_unique_id = "ic_student"
         self._hub = hub
         self._hass = hass
+        self._attr_json = "test"
+
+    @property
+    def extra_state_attributes(self):
+        return {
+            "json": self._attr_json
+        }
 
     async def async_update(self) -> str:
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._attr_native_value = await self._hub.poll_data("student")
+        self._attr_json = await self._hub.poll_data("student")
 
 class InfiniteCourseSensor(SensorEntity):
     def __init__(
         self,
-        unique_id
+        hass: HomeAssistant,
+        hub: infinitecampus.InfiniteHub
     ) -> None:
-        self._attr_unique_id = unique_id
         self._attr_name = "Infinite Campus Course"
         self._attr_native_unit_of_measurement = None
         self._attr_device_class = None
         self._attr_state_class = None
+        self._attr_unique_id = "ic_course"
+        self._hub = hub
+        self._hass = hass
+        self._attr_json = "test"
 
     async def async_update(self) -> None:
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._attr_native_value = 23
+        self._attr_json = await self._hub.poll_data("course")
 
 class InfiniteAssignmentSensor(SensorEntity):
     def __init__(
         self,
-        unique_id
+        hass: HomeAssistant,
+        hub: infinitecampus.InfiniteHub
     ) -> None:
-        self._attr_unique_id = unique_id
         self._attr_name = "Infinite Campus Assignment"
         self._attr_native_unit_of_measurement = None
         self._attr_device_class = None
         self._attr_state_class = None
+        self._attr_unique_id = "ic_assignment"
+        self._hub = hub
+        self._hass = hass
+        self._attr_json = "test"
 
     async def async_update(self) -> None:
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._attr_native_value = 23
+        self._attr_json = await self._hub.poll_data("assignment")
