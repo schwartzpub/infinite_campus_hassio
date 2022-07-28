dab16081ea08b0232efcbd7d52a53ae259ae2a30
schwartzpub <jake@schwartzpub.com>
2022-07-28 09:30:45 -0500

    update platform

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ from .const import (
     CONF_DISTRICT,
     CONF_SECRET,
     CONF_USERNAME,
-    PLATFORMS,
+    HA_SENSOR,
     DOMAIN
 )
 
@@ -24,14 +24,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         hass
     )
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    hass.config_entries.async_setup_platforms(entry, HA_SENSOR)
 
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, HA_SENSOR):
         hass.data[DOMAIN].pop(entry.entry_id)
 
     return unload_ok
