197305ff9af9d51eb389643bd0280c14c2810dee
schwartzpub <jake@schwartzpub.com>
2022-07-28 12:33:17 -0500

    more logs

--- __init__.py
+++ __init__.py
@@ -30,7 +30,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = InfiniteHub(hass)
     _LOGGER.warning("-------SETTING UP PLATFORMS--------")
     hass.config_entries.async_setup_platforms(entry, HA_SENSOR)
-
+    _LOGGER.warning("-------COMPLETED SETTING UP PLATFORMS--------")
     return True
 
 
