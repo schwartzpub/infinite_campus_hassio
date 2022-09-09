045d0fe1ee75a216a867bebb87f3b8efff089b12
jake@schwartzpub.com <jake@schwartzpub.com>
2022-09-08 19:43:39 -0500

    add initial data fetch

--- sensor.py
+++ sensor.py
@@ -3,8 +3,13 @@ from __future__ import annotations
 
 import logging
 
-from homeassistant.components.sensor import SensorEntity
+from collections.abc import Callable
+from dataclasses import dataclass
+
+from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
 from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN, SCAN_INT
 
@@ -13,127 +18,85 @@ _LOGGER = logging.getLogger(__name__)
 SCAN_INTERVAL = SCAN_INT
 
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+@dataclass
+class InfiniteCampusEntityDescriptionMixIn:
+    """Mixin for required keys."""
+
+    value_fn: Callable
+    unique_id: str
+
+
+@dataclass
+class InfiniteCampusEntityDescription(
+    SensorEntityDescription, InfiniteCampusEntityDescriptionMixIn
+):
+    """Describes Infinite Campus sensor entity."""
+
+
+SENSORS: tuple[InfiniteCampusEntityDescription, ...] = (
+    InfiniteCampusEntityDescription(
+        key="student",
+        name="Infinite Campus Students",
+        unique_id="ic_student",
+        value_fn=lambda canvas: canvas.poll_students()
+    ),
+    InfiniteCampusEntityDescription(
+        key="course",
+        name="Infinite Campus Courses",
+        unique_id="ic_course",
+        value_fn=lambda canvas: canvas.poll_courses()
+    ),
+    InfiniteCampusEntityDescription(
+        key="assignment",
+        name="Infinite Campus Assignments",
+        unique_id="ic_assignment",
+        value_fn=lambda canvas: canvas.poll_assignments()
+    ),
+    InfiniteCampusEntityDescription(
+        key="term",
+        name="Infinite Campus Terms",
+        unique_id="ic_term",
+        value_fn=lambda canvas: canvas.poll_terms()
+    )
+)
+async def async_setup_entry(
+    hass: HomeAssistant, 
+    config_entry: ConfigEntry, 
+    async_add_entities: AddEntitiesCallback
+    ):
     """Set up the sensor platform."""
     hub = hass.data[DOMAIN][config_entry.entry_id]
 
     async_add_entities(
-        [
-            InfiniteStudentSensor(hass, hub),
-            InfiniteCourseSensor(hass, hub),
-            InfiniteAssignmentSensor(hass, hub),
-            InfiniteTermSensor(hass, hub),
-        ]
+        [InfiniteCampusSensor(description, hub) for description in SENSORS],
+        True,
     )
 
 
-class InfiniteStudentSensor(SensorEntity):
-    """Infinite Campus Student entity definition."""
+class InfiniteCampusSensor(SensorEntity):
+    """Infinite Campus Sensor Definition."""
+    entity_description: InfiniteCampusEntityDescription
 
-    def __init__(self, hass: HomeAssistant, hub) -> None:
-        """Init Student sensor."""
-        self._attr_name = "Infinite Campus Students"
-        self._attr_native_unit_of_measurement = None
-        self._attr_device_class = None
-        self._attr_state_class = None
-        self._attr_unique_id = "ic_student"
+    def __init__(
+        self,
+        description: InfiniteCampusEntityDescription,
+        hub
+    ) -> None:
         self._hub = hub
-        self._hass = hass
-        self._attr_students = {}
+        self._attr_name = description.name
+        self._attr_unique_id = f"{description.unique_id}"
+        self._entity_description = description
+        self._attr_infinite_campus_data = {}    
 
     @property
     def extra_state_attributes(self):
-        """Set up extra attributes."""
-        return {"students": [x.as_dict() for x in self._attr_students]}
+        """Add extra attribute."""
+        return {f"{self._entity_description.key}": [x.as_dict() for x in self._attr_infinite_campus_data]}
 
     async def async_update(self) -> None:
         """Fetch new state data for the sensor.
 
         This is the only method that should fetch new data for Home Assistant.
         """
-        self._attr_students = await self._hub.poll_students()
-        return
-
-
-class InfiniteCourseSensor(SensorEntity):
-    """Infinite Campus Course entity definition."""
-
-    def __init__(self, hass: HomeAssistant, hub) -> None:
-        """Init Course sensor."""
-        self._attr_name = "Infinite Campus Courses"
-        self._attr_native_unit_of_measurement = None
-        self._attr_device_class = None
-        self._attr_state_class = None
-        self._attr_unique_id = "ic_course"
-        self._hub = hub
-        self._hass = hass
-        self._attr_courses = {}
-
-    @property
-    def extra_state_attributes(self):
-        """Add extra attributes."""
-        return {"courses": [x.as_dict() for x in self._attr_courses]}
-
-    async def async_update(self) -> None:
-        """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        self._attr_courses = await self._hub.poll_courses()
-        return
-
-
-class InfiniteAssignmentSensor(SensorEntity):
-    """Infinite Campus Assignment entity definition."""
-
-    def __init__(self, hass: HomeAssistant, hub) -> None:
-        """Init Assignment Sensor."""
-        self._attr_name = "Infinite Campus Assignments"
-        self._attr_native_unit_of_measurement = None
-        self._attr_device_class = None
-        self._attr_state_class = None
-        self._attr_unique_id = "ic_assignment"
-        self._hub = hub
-        self._hass = hass
-        self._attr_assignments = {}
-
-    @property
-    def extra_state_attributes(self):
-        """Add extra attributes."""
-        return {"assignments": [x.as_dict() for x in self._attr_assignments]}
-
-    async def async_update(self) -> None:
-        """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        self._attr_assignments = await self._hub.poll_assignments()
-        return
-
-
-class InfiniteTermSensor(SensorEntity):
-    """Infinite Campus Term entity definition."""
-
-    def __init__(self, hass: HomeAssistant, hub) -> None:
-        """Init Term Sensor."""
-        self._attr_name = "Infinite Campus Terms"
-        self._attr_native_unit_of_measurement = None
-        self._attr_device_class = None
-        self._attr_state_class = None
-        self._attr_unique_id = "ic_term"
-        self._hub = hub
-        self._hass = hass
-        self._attr_terms = {}
-
-    @property
-    def extra_state_attributes(self):
-        """Add extra attributes."""
-        return {"terms": [x.as_dict() for x in self._attr_terms]}
-
-    async def async_update(self) -> None:
-        """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        self._attr_terms = await self._hub.poll_terms()
+        self._attr_infinite_campus_data = await self._entity_description.value_fn(self._hub)
         return
